3b3863130cd7cf90595ab6c0117d2f704356adcb
Jonathan Salazar <jonsalaz23@gmail.com>
2022-08-07 20:48:50 -0600

    Create Contact Form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,12 @@
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
+import ContactForm from "./ContactForm";
 
 function Contact() {
     return (  
         <>
             <Header />
+            <ContactForm />
             <Footer />
         </>
      );
---
+++ src/components/Contact/ContactForm.js
@@ -0,0 +1,77 @@
+import styled from "@emotion/styled";
+import {
+    Button,
+    Divider,
+    InputAdornment,
+    Stack,
+    TextField,
+    Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import { useState } from "react";
+import SearchIcon from "@mui/icons-material/Search";
+
+function ContactForm() {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+
+    return (
+        <Box
+            maxWidth="50%"
+            color="text.secondary"
+            sx={{
+                input: { color: "text.secondary" },
+                textArea: { color: "text.secondary" },
+            }}
+        >
+            <Typography variant="h4" padding="10px">
+                Contact Me
+            </Typography>
+            <Stack spacing={2}>
+                <Stack
+                    direction="row"
+                    spacing={4}
+                    justifyContent="space-between"
+                >
+                    <TextField
+                        variant="standard"
+                        required
+                        label="First Name"
+                        autoComplete="off"
+                        onChange={(e) => setFirstName(e.target.value)}
+                    />
+
+                    <TextField
+                        required
+                        type="text"
+                        label="Last Name"
+                        variant="standard"
+                        onChange={(e) => {
+                            setLastName(e.target.value);
+                        }}
+                    />
+                    <TextField
+                        required
+                        type="email"
+                        label="Email"
+                        variant="standard"
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                        }}
+                    />
+                </Stack>
+                <TextField
+                    required
+                    multiline
+                    label="Email Contents"
+                    variant="standard"
+                    fullWidth
+                />
+                <Button variant="contained">Submit</Button>
+            </Stack>
+        </Box>
+    );
+}
+
+export default ContactForm;
