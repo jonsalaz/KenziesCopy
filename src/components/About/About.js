57db659bcbc5625e7843cc4146d3a354d1955031
Jonathan Salazar <jonsalaz23@gmail.com>
2022-08-08 13:43:33 -0600

    Formatting

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,19 +4,19 @@ import Header from "../Header/Header";
 import AboutContent from "./AboutContent/AboutContent";
 
 function About() {
-    return ( 
+    return (
         <Box
             sx={{
                 bgcolor: "background.main",
                 minHeight: "100vh",
                 width: "100vw",
             }}
-        >  
-        <Header />
-        <AboutContent />
-        <Footer />
+        >
+            <Header />
+            <AboutContent />
+            <Footer />
         </Box>
-     );
+    );
 }
 
 export default About;
