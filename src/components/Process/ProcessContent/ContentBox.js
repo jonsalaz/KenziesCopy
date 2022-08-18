409a4ce9d35398f7323f21d75ce55f2609d0df80
Jonathan Salazar <jonsalaz23@gmail.com>
2022-08-17 20:51:10 -0600

    Text Sizing

--- src/components/Process/ProcessContent/ContentBox.js
+++ src/components/Process/ProcessContent/ContentBox.js
@@ -4,7 +4,7 @@ function ContentBox(props) {
     return ( 
         <Box>
             <Typography variant="h4">{props.header}</Typography>
-            <Typography variant="body1">{props.content}</Typography>
+            <Typography variant="h6">{props.content}</Typography>
         </Box>
      );
 }
