ab706b9e43bfa0dc1d781f39f9f7b3071b735d11
Jonathan Salazar <jonsalaz23@gmail.com>
2022-08-17 21:05:43 -0600

    Set Background Image of Contact Page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,15 +1,22 @@
+import { Box } from "@mui/material";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import ContactForm from "./ContactForm";
 
+import bg from "./bg.jpg";
+
 function Contact() {
-    return (  
-        <>
+    return (
+        <Box
+            height="100vh"
+            width="100vw"
+            sx={{ backgroundImage: `url(${bg})`, backgroundSize: "cover" }}
+        >
             <Header />
             <ContactForm />
             <Footer />
-        </>
-     );
+        </Box>
+    );
 }
 
 export default Contact;
