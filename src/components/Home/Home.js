09db56c94ea1fb70d2619ebafcb9732d9ed74fbc
Jonathan Salazar <jonsalaz23@gmail.com>
2022-08-17 20:33:17 -0600

    Spacing Adjustments

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -40,49 +40,48 @@ function Home() {
                 </Typography>
             </Box>
 
-            <Container
-                disableGutters
-                maxWidth="xl"
-                sx={{
-                    display: "flex",
-                    flexDirection: "row",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    padding: "50px"
-                }}
-            >
-                <Typography variant="h5" width="33%">
-                    Kenzie’s background as an actor, reader/writer, and
-                    Communications student renders her abundantly capable of
-                    <b> fostering connection, capturing attention,</b>
-                    and<b> tapping into the truth;</b> discovering what your
-                    audience really wants, and which messages will move them.
-                </Typography>
-
-                <Paper
-                    component="img"
-                    src={portrait}
-                    elevation={24}
-                    sx={{ margin: "10px 10px 10px 0px" }}
-                />
-
-                <Stack width="33%" spacing={8}>
-                    <Typography variant="h5">
-                        They say acting is all about the "other person"—in this
-                        case, that “other person” is you.
+            <Container width="100vw">
+                <Stack
+                    direction="row"
+                    spacing={4}
+                    justifyContent="center"
+                    alignItems="center"
+                    padding="50px 0 50px 0"
+                >
+                    <Typography variant="h5" width="33%">
+                        Kenzie’s background as an actor, reader/writer, and
+                        Communications student renders her abundantly capable of
+                        <b> fostering connection, capturing attention,</b>
+                        and<b> tapping into the truth;</b> discovering what your
+                        audience really wants, and which messages will move
+                        them.
                     </Typography>
-                    <Typography variant="h5">
-                        You have a business, a product, something to say—but
-                        maybe you don’t have the words. Kenzie does have the
-                        words—
-                        <b>
-                            <i>and she wants to tell your story.</i>
-                        </b>
-                    </Typography>
+
+                    <Paper
+                        component="img"
+                        src={portrait}
+                        elevation={24}
+                        sx={{ margin: "10px 10px 10px 0px" }}
+                    />
+
+                    <Stack width="33%" spacing={8}>
+                        <Typography variant="h5">
+                            They say acting is all about the "other person"—in
+                            this case, that “other person” is you.
+                        </Typography>
+                        <Typography variant="h5">
+                            You have a business, a product, something to say—but
+                            maybe you don’t have the words. Kenzie does have the
+                            words—
+                            <b>
+                                <i>and she wants to tell your story.</i>
+                            </b>
+                        </Typography>
+                    </Stack>
                 </Stack>
             </Container>
             <ContactCard />
-            <div style={{padding: "25px"}}>{null}</ div>
+            <div style={{ padding: "5px" }}>{null}</div>
             <Footer />
         </Box>
     );
